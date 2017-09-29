2e16f614ca316f92ec64f30617c715e440a61d79
ALRUTEN <laurentnavarro31@gmail.com>
2017-09-29 17:26:58 +0200

    dojo

--- Average.java
+++ Average.java
@@ -3,11 +3,10 @@ class Average {
 	public static float average(int[] values) {
 		float total = 0;
 
-		// début de ton code ici !
+		for(int i = 0;i < values.length;i++){
+			total = total + values[i];}
 
 
-
-		// fin de ton code :)
 
 		return total / values.length;
 	}
